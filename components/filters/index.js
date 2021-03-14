c9317d4eea110d15e898bccdb53441517fd05bc9
Shubham Khandelwal <khandelwalshubham1992@gmail.com>
2021-03-14 16:12:39 +0530

    Removed console.logs

--- components/filters/index.js
+++ components/filters/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from './Filters.module.css';
 
 const Filters = ({ heading, _key, filterData, selected }) => {
-  console.log(selected);
-  console.log(filterData[0]);
   return <article>
     <header>
       {heading}
@@ -11,7 +9,6 @@ const Filters = ({ heading, _key, filterData, selected }) => {
     <hr />
     <div className={styles.filterCard}>
       {filterData.map(data => {
-        console.log(data === selected);
         return <button key={data} className={data === selected ? styles.selected : undefined}>{data}</button>
       })}
     </div>
